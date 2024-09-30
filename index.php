ce866c01373a4681c1e9bc42eb8e3a5dc76e3337
Kalef22 <144925042+Kalef22@users.noreply.github.com>
2024-09-30 22:38:15 +0200

    creando la estructura del sitio web

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Joyería Online</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>Joyería OnLine</h1>
+    </header>
+
+    <nav>
+        <a href="">Menu 1</a>
+        <a href="">Menu 2</a>
+        <a href="">Menu 3</a>
+    </nav>
+
+    <section>
+        <article>
+            <h2>Titulo del artículo</h2>
+            <p>Contenido del artículo</p>
+            <div>
+                <img src="nombre de la imagen" alt="">
+            </div>
+        </article>
+    </section>
+
+    <aside>
+        <h3>Publicidad</h3>
+        <p>Contenido de la publicidad</p>
+    </aside>
+
+    <footer>
+        <p>Avisos legales - Copyrigth 2024 - Mapa del sitio</p>
+    </footer>
+
+</body>
+
+</html>
