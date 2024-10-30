30d29467dbe2820d9572a1952b99c1caa99e7ec3
Kalef22 <144925042+Kalef22@users.noreply.github.com>
2024-10-30 01:19:03 +0100

    modificando a la estructura MVC

--- bootstrap-shop/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-const DB_HOST="localhost";
-const DB_NAME="tienda_online";
-const DB_USER="admin";
-const DB_PASS="NstF2@O@U6yBqaF6";
-?>
---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+const DB_HOST = "localhost";
+const DB_NAME = "tienda_online";
+const DB_USER = "admin";
+const DB_PASS = "NstF2@O@U6yBqaF6";
