97788af8280b47192f31a87c763a6072a22b474c
Kalef22 <144925042+Kalef22@users.noreply.github.com>
2024-11-25 18:34:57 +0100

    prueba

---
+++ enrutamientpo.php
@@ -0,0 +1,50 @@
+<?php
+// --------------------- ENRUTADOR CON SOPORTE PARA CONTROLADORES ---------------------
+
+// Obtiene las rutas solicitadas
+$request_uri = $_SERVER['REQUEST_URI'];
+// Obtiene la ruta base
+$base_path = "/";
+// Elimina la ruta base y contendrá la parte de la URI que sigue a la ruta base,
+$ruta = str_replace($base_path, '', $request_uri);
+// Obtener la parte de la ruta antes del primer signo de interrogación ?
+$ruta = strtok($route, '?');
+
+
+// Función para cargar controladores dinámicamente
+function cargarControlador($controller, $action = 'index') {
+    $controller_file = "controllers/{$controller}.php";
+
+    if (file_exists($controller_file)) {
+        include $controller_file;
+        if (function_exists($action)) {
+            $action(); // Llama a la acción dentro del controlador
+        } else {
+            die("Acción no encontrada: $action");
+        }
+    } else {
+        echo "error cargar controlador";
+        include 'views/404.php'; // Archivo no encontrado
+       
+    }
+}
+
+
+// Tabla de rutas 
+$rutas = [
+    '/' => ['informacionController', 'index'],
+    'views/usuarios/login' => ['usuariosController', 'login'],
+    'views/usuarios/register' => ['usuariosController', 'register'],
+    'views/informacion/sobreNosotros' => ['informacionController', 'sobreNosotros'],
+    'views/informacion/contacto' => ['informacionController', 'contacto'],
+];
+
+// Procesar la ruta
+if (array_key_exists($ruta, $rutas)) {
+    $controller = $rutas[$ruta][0];
+    $action = $rutas[$ruta][1];
+    cargarControlador($controller, $action);
+} else {
+    echo "error array_key_exists";
+    include 'views/404.php';
+}
--- index.php
+++ index.php
@@ -1,50 +1,40 @@
 <?php
-// --------------------- ENRUTADOR CON SOPORTE PARA CONTROLADORES ---------------------
-
-// Obtiene las rutas solicitadas
-$request_uri = $_SERVER['REQUEST_URI'];
-// Obtiene la ruta base
-$base_path = "/";
-// Elimina la ruta base y contendrá la parte de la URI que sigue a la ruta base,
-$ruta = str_replace($base_path, '', $request_uri);
-// Obtener la parte de la ruta antes del primer signo de interrogación ?
-$ruta = strtok($route, '?');
-
-
-// Función para cargar controladores dinámicamente
-function cargarControlador($controller, $action = 'index') {
-    $controller_file = "controllers/{$controller}.php";
-
-    if (file_exists($controller_file)) {
-        include $controller_file;
-        if (function_exists($action)) {
-            $action(); // Llama a la acción dentro del controlador
-        } else {
-            die("Acción no encontrada: $action");
+try{
+    $query_listar ="SELECT id_cliente, nombre, apellido1, apellido2, email, rol FROM cliente";
+    $stmt_listar = $this->pdo->prepare($query_listar);
+    $stmt_listar->execute();
+    $usuarios = $stmt_listar->fetchAll(PDO::FETCH_ASSOC);
+    if(count($usuarios)>0){
+        echo "<table>
+                <thead>
+                    <tr>
+                        <th>id_usuario</th>
+                        <th>Nombre</th>
+                        <th>Primer apellido</th>
+                        <th>Segundo apellido</th>
+                        <th>Email</th>
+                        <th>Rol</th>
+                    </tr>
+                </thead>
+                <tbody>";
+        foreach($usuarios as $usuario){
+            echo "<tr>
+                    <td>".$usuario['id_usuario']."</td>
+                    <td>".$usuario['nombre']."</td>
+                    <td>".$usuario['apellido1']."</td>
+                    <td>".$usuario['apellido2']."</td>
+                    <td>".$usuario['email']."</td>
+                    <td>".$usuario['rol']."</td>
+                    </tr>";
         }
-    } else {
-        echo "error cargar controlador";
-        include 'views/404.php'; // Archivo no encontrado
-       
+        echo "</tbody>
+            </table>";
+    }else{
+        echo "No hay usuarios registrados";
     }
+} catch(PDOException $e){
+    echo "Error al listar usuarios: ".$e->getMessage();
+    return false;
 }
 
-
-// Tabla de rutas 
-$rutas = [
-    '/' => ['informacionController', 'index'],
-    'views/usuarios/login' => ['usuariosController', 'login'],
-    'views/usuarios/register' => ['usuariosController', 'register'],
-    'views/informacion/sobreNosotros' => ['informacionController', 'sobreNosotros'],
-    'views/informacion/contacto' => ['informacionController', 'contacto'],
-];
-
-// Procesar la ruta
-if (array_key_exists($ruta, $rutas)) {
-    $controller = $rutas[$ruta][0];
-    $action = $rutas[$ruta][1];
-    cargarControlador($controller, $action);
-} else {
-    echo "error array_key_exists";
-    include 'views/404.php';
-}
+?>
